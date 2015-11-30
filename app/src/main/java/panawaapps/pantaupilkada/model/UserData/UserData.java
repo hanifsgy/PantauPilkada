68111266725a54cb2e08cb459ac40dd8ac91441d
hanifsgy <hanifsgy@gmail.com>
2015-11-30 19:47:21 +0700

    adding UserData.java

---
+++ app/src/main/java/panawaapps/pantaupilkada/model/UserData/UserData.java
@@ -0,0 +1,61 @@
+package panawaapps.pantaupilkada.model.UserData;
+
+/**
+ * Created by ali on 26/11/15.
+ */
+public class UserData {
+    private String email;
+    private String password;
+    private String password_confirmation;
+    private String name;
+    private String dob;
+    private String address;
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getPassword_confirmation() {
+        return password_confirmation;
+    }
+
+    public void setPassword_confirmation(String password_confirmation) {
+        this.password_confirmation = password_confirmation;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDob() {
+        return dob;
+    }
+
+    public void setDob(String dob) {
+        this.dob = dob;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+}
